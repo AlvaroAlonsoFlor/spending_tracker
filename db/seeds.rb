4a4c3f4899cb96728f967cac84564d9ea735895b
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-24 20:07:29 +0100

    Add seeds, problem with transaction2

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,56 @@ require_relative('../models/merchant')
 require_relative('../models/tag')
 require_relative('../models/transaction')
 
+# MERCHANTS
+
 tesco = Merchant.new({
   'name' => 'Tesco'
+  })
+
+aldi = Merchant.new({
+  'name' => 'Aldi'
   })
 lothian_buses = Merchant.new({
   'name' => 'Lothian Buses'
   })
+
+scotrail = Merchant.new({
+  'name' => 'Scotrail'
+  })
+
+tesco.save
+lothian_buses.save
+scotrail.save
+aldi.save
+
+# TAGS
+
+food = Tag.new({
+  'category' => 'food'
+  })
+
+transport = Merchant.new({
+  'category' => 'transport'
+  })
+
+food.save
+transport.save
+
+# TRANSACTIONS
+
+transaction1 = Transaction.new({
+  'description' => 'weekly shopping',
+  'merchant_id' => aldi.id,
+  'tag_id' => food.id,
+  'amount' => 23.17
+  })
+
+transaction2 = Transaction.new({
+  'description' => 'monthly Riddacard',
+  'merchant_id' => lothian_buses.id,
+  'tag_id' => transport.id,
+  'amount' => 53
+  })
+
+transaction1.save
+transaction2.save
