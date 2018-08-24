2a882e026a05c95c0fe0642f2f1046150354d87c
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-24 15:16:45 +0100

    Add tests for Transaction, atributes and total

---
+++ models/specs/transaction_specs.rb
@@ -0,0 +1,38 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../transaction')
+
+class TestTag < MiniTest::Test
+  def setup
+    @transaction1 = Transaction.new({
+
+      'description' => 'chocolate'
+      'merchant_id' => 1
+      'tag_id' => 1
+      'amount' => 1
+    })
+
+    @transaction2 = Transaction.new({
+
+      'description' => 'cinema ticket'
+      'merchant_id' => 3
+      'tag_id' => 2
+      'amount' => 10
+    })
+
+  end
+
+  def test_has_a_description
+    assert_equal("chocolate", @transaction1.description )
+  end
+
+  def test_has_an_amount
+    assert_equal(1, @transaction1.amount )
+  end
+
+  def test_total_amount
+    assert_equal(11, Transaction.total)
+  end
+
+
+end
