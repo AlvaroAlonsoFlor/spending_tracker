18f70b6ae399318d8e5aef9d94abbc9cee4d9958
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-26 20:10:18 +0100

    Add new route to transactions, error with end of input

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/transaction')
 require_relative('../models/tag')
-require_relative('../models/merchant')
+require_relative('../models/transaction')
 also_reload('../models/*')
 
 # INDEX
@@ -10,4 +10,12 @@ also_reload('../models/*')
 get '/transactions' do
   @transactions = Transaction.all
   erb(:'transactions/index')
+end
+
+# NEW
+
+get '/transactions/new' do
+  @merchants = Merchant.all
+  @tags = Tag.all
+  erb(:'transactions/new')
 end
