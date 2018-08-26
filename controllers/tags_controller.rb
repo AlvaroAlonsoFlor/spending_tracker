0c2d376b669e4ebaea30487edfdf8e15b8a9ba69
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-26 14:25:36 +0100

    Add view files for tags

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -3,3 +3,10 @@ require('sinatra/contrib/all')
 require_relative('../models/tag')
 also_reload('../models/*')
 require_relative('../models/tag')
+
+#INDEX
+
+get '/tags' do
+  @tags = Tags.all
+  erb(:'tags/index')
+end
