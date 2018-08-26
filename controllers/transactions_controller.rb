d2e74d80ea84568afa0f499bbd33092ff5a25bcc
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-26 19:45:49 +0100

    Add index route for transactions

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -4,3 +4,10 @@ require_relative('../models/transaction')
 require_relative('../models/tag')
 require_relative('../models/merchant')
 also_reload('../models/*')
+
+# INDEX
+
+get '/transactions' do
+  @transactions = Transaction.all
+  erb(:'transactions/index')
+end
