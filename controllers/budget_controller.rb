c47d9195168faf008fdc2c80fb90ae2e7e37b9c8
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-28 17:26:06 +0100

    Add relative path including budget controller to app.rb

--- controllers/budget_controller.rb
+++ controllers/budget_controller.rb
@@ -3,3 +3,21 @@ require('sinatra/contrib/all')
 require_relative('../models/transaction')
 require_relative('../models/budget')
 also_reload('../models/*')
+
+# INDEX
+
+get '/budgets' do
+  "hello"
+end
+
+# SHOW
+
+# NEW
+
+# CREATE
+
+# EDIT
+
+# UPDATE
+
+# DELETE
