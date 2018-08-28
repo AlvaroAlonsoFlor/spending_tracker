6743ddf1f8f071efff488bfc7965b1072f5a3820
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-28 12:51:02 +0100

    Add save method to budget

--- models/budget.rb
+++ models/budget.rb
@@ -9,4 +9,16 @@ class Budget
     @start_date = info['start_date']
     @finish_date = info['finish_date']
   end
+
+  def save
+    sql = "INSERT INTO budgets
+    (name, amount, start_date, finish_date)
+    VALUES
+    ($1, $2, $3, $4)
+    RETURNING id
+    "
+    values =[@name, @amount, @start_date, @finish_date]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
+  end
 end
