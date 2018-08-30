b7db36fa13ec8045eea31ae671417428cc2aad10
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-30 11:13:53 +0100

    modify pg.connect for heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,11 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'spending_tracker', host: 'localhost' })
+      # local
+      # db = PG.connect({ dbname: 'spending_tracker', host: 'localhost' })
+
+      # Heroku
+      db = PG.connect({ dbname: 'daapnvivt153p7', host: 'ec2-174-129-236-147.compute-1.amazonaws.com', port: '5432', user: 'ozfeqtlwraunms', password: '0aaffa677aa1ffda37acac175eaff1cde19e44a7d1d83a7d72338cd3637417b7' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
